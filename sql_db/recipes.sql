3de30e274d89995960fc1c9eb054760f9f946ce1
Nathan Muncy <nmuncy2@unl.edu>
2024-12-02 13:22:24 -0600

    [DEV] adding tables for afq scalars

--- sql_db/recipes.sql
+++ sql_db/recipes.sql
@@ -219,3 +219,56 @@ create table tbl_test_dates(
     foreign key(subj_id) references ref_subj(subj_id) on delete cascade,
     foreign key(test_id) references ref_test(test_id)
 );
+
+-- pyAFQ tract metrics
+create table ref_afq(
+    tract_id int not null,
+    tract_name varchar(31),
+    primary key(tract_id)
+);
+insert into ref_afq
+    (tract_id, tract_name)
+    values
+    (1, "Left Anterior Thalamic"),
+    (2, "Right Anterior Thalamic"),
+    (3, "Left Cingulum Cingulate"),
+    (4, "Right Cingulum Cingulate"),
+    (5, "Left Corticospinal"),
+    (6, "Right Corticospinal"),
+    (7, "Left Inferior Fronto-occipital"),
+    (8, "Right Inferior Fronto-occipital"),
+    (9, "Left Inferior Longitudinal"),
+    (10, "Right Inferior Longitudinal"),
+    (11, "Left Superior Longitudinal"),
+    (12, "Right Superior Longitudinal"),
+    (13, "Left Arcuate"),
+    (14, "Right Arcuate"),
+    (15, "Left Uncinate"),
+    (16, "Right Uncinate"),
+    (17, "Left Posterior Arcuate"),
+    (18, "Right Posterior Arcuate"),
+    (19, "Left Vertical Occipital"),
+    (20, "Right Vertical Occipital"),
+    (21, "Callosum Anterior Frontal"),
+    (22, "Callosum Motor"),
+    (23, "Callosum Occipital"),
+    (24, "Callosum Orbital"),
+    (25, "Callosum Posterior Parietal"),
+    (26, "Callosum Superior Frontal"),
+    (27, "Callosum Superior Parietal"),
+    (28, "Callosum Temporal");
+
+create table tbl_afq(
+    subj_id int not null,
+    sess_id int not null,
+    tract_id int not null,
+    node_id int not null,
+    dti_fa numeric(17,16),
+    dti_md numeric(17,16),
+    dti_ad numeric(17,16),
+    dti_rd numeric(17,16),
+    primary key(subj_id, sess_id, tract_id, node_id),
+    foreign key(subj_id) references ref_subj(subj_id) on delete cascade,
+    foreign key(sess_id) references ref_scan(scan_id),
+    foreign key(tract_id) references ref_afq(tract_id)
+);

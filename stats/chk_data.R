b387918cbfbb88115522a4e31b614f5916336c0c
Nathan Muncy <nmuncy2@unl.edu>
2024-12-03 14:55:13 -0600

    [DEV] working on GAM specification

--- stats/chk_data.R
+++ stats/chk_data.R
@@ -14,12 +14,12 @@ df <- get_afq()
 
 
 # Get Parietal data---- 
+#
+# TODO fit all tracts in same model
 tract <- "Callosum Superior Parietal"
+
 df_par <- df[
-  which(
-    df$tract_name == tract
-    & df$sess_name %in% c("base", "post")
-  ),
+  which(df$tract_name == tract & df$sess_name %in% c("base", "post")),
   c("subj_id", "sess_name", "node_id", "dti_fa")
 ]
 df_par$subj_id <- factor(df_par$subj_id)
@@ -28,78 +28,71 @@ df_par <- df_par[order(df_par$subj_id, df_par$sess_name, df_par$node_id),]
 df_par <- df_par[which(df_par$node_id > 9 & df_par$node_id < 91), ]
 
 
-# Fit data with global and group smooths ----
+# Fit base FA data ----
 #
-# Specify a GS model, determine
-# which family best fits data.
-#   - Gaussian fit better.
-hist(df_par$dti_fa)
-descdist(df_par$dti_fa, discrete = F)
+# Strong fit, explaining 87.6% of deviance.
+# Using method = "REML" did not change fit,
+# and bs="cr" did not change fit.
+df_base <- df_par[which(df_par$sess_name == "base"),]
+hist(df_base$dti_fa)
+descdist(df_base$dti_fa, discrete = F) # Will use beta dist
 
-fit_norm <- bam(
-  dti_fa ~ s(subj_id, bs="re") + 
-    s(node_id, bs="cr", k=60, m=2) +
-    s(node_id, sess_name, bs="fs", k=60, m=2),
-  data = df_par,
-  family = gaussian(),
-  method = "fREML"
+fit_G <- bam(
+  dti_fa ~ s(node_id, bs="tp", k=40) + s(subj_id, bs="re"),
+  data = df_base,
+  family = betar(link="logit"),
+  method = "fREML",
+  discrete = T
 )
-gam.check(fit_norm, rep=1000)
-summary(fit_norm)
+gam.check(fit_G, rep=1000)
+summary(fit_G)
+plot(fit_G)
 
-fit_beta <- bam(
-  dti_fa ~ s(subj_id, bs="re") + 
-    s(node_id, bs="cr", k=70, m=2) +
-    s(node_id, sess_name, bs="fs", k=70, m=2),
+
+# Fit FA data by base, post sessions ----
+#
+# Ordered factor uses sets base to zero,
+# checks for flatness of post.
+#
+# Smooth of node_id:sess_name f=2.079,
+# dev expl = 87.3%.
+#
+# Smooth of node_id:sessOFpost f=15.69,
+# dev expl = 87.3%.
+fit_S <- bam(
+  dti_fa ~ s(node_id, bs="tp", k=40, m=2) + 
+    s(subj_id, bs="re") +
+    s(node_id, sess_name, bs="fs", k=40, m=2),
   data = df_par,
   family = betar(link="logit"),
-  method = "fREML"
+  method = "fREML",
+  discrete = T
 )
-gam.check(fit_beta, rep=1000)
-summary(fit_beta)
+summary(fit_S)
+plot(fit_S)
 
-compareML(fit_norm, fit_beta)
-rm(fit_beta)
-plot(fit_norm)
-
-
-# Compare GS with GI model ----
-#
-# Adding group wiggliness did not improve fit.
-fit_GI <- bam(
-  dti_fa ~ s(subj_id, bs="re") + 
-    s(sess_name, bs="re") +
-    s(node_id, bs="cr", k=60, m=2) +
-    s(node_id, by = sess_name, bs="cr", k=60, m=1),
-  data = df_par,
-  family = gaussian(),
-  method = "fREML"
-)
-summary(fit_GI)
-compareML(fit_norm, fit_GI)
-rm(fit_GI)
+plot_S <- getViz(fit_S)
+pGlobal <- draw_global_smooth(plot_S, 1, tract)
+pGroup <- draw_group_smooth(plot_S, 3, tract)
 
 
-# Test for group smooth differences ----
 df_par$sessOF <- factor(df_par$sess_name, ordered = T)
-fit_OF <- bam(
-  dti_fa ~ s(subj_id, bs="re") + 
-    s(node_id, bs="cr", k=60, m=2) +
-    s(node_id, by = sessOF, bs="cr", k=60, m=2),
+fit_SO <- bam(
+  dti_fa ~ s(node_id, bs="tp", k=40, m=2) + 
+    s(subj_id, bs="re") +
+    s(node_id, by = sessOF, bs="fs", k=40, m=2),
   data = df_par,
-  family = gaussian(),
-  method = "fREML"
+  family = betar(link="logit"),
+  method = "fREML",
+  discrete = T
 )
-summary(fit_OF)
-plot(fit_OF)
+summary(fit_SO)
+plot(fit_SO)
 
-plot_norm <- getViz(fit_norm)
-pGlobal <- draw_global_smooth(plot_norm, 2, tract)
-pGroup <- draw_group_smooth(plot_norm, 3, tract)
+plot_SO <- getViz(fit_SO)
+pDiff <- draw_group_smooth_diff(plot_SO, 3, tract)
 
-plot_tract_GSOF <- getViz(fit_OF)
-pDiff <- draw_group_smooth_diff(plot_tract_GSOF, 3, tract)
-
+# draw grid
 plot_list <- list(
   "global" = pGlobal,
   "group" = pGroup,
@@ -114,3 +107,66 @@ name_list <- list(
   "bot1" = "Tract Node"
 )
 draw_one_three(plot_list, name_list, tract)
+
+
+# Fit as longitudinal model ----
+#
+# Works when using three sessions (base, post, rtp),
+# but gives smooth for e/ sess*subj which is
+# not of interest.
+#
+# References examples at:
+#   - https://stats.stackexchange.com/questions/565460/is-my-gam-modeling-longitudinal-change-appropriately
+#   - https://stats.stackexchange.com/questions/462932/how-do-i-correctly-specify-a-gamm-formula-to-model-interactions-of-random-and-fi
+#   - https://stats.stackexchange.com/questions/391912/how-to-fit-a-longitudinal-gam-mixed-model-gamm
+df_par$sess <- as.numeric(df_par$sess_name)
+fit_X <- bam(
+  dti_fa ~ s(node_id, bs="tp", k=40, m=2) + 
+    s(sess, subj_id, bs="fs", k=2) +
+    s(node_id, sess, bs="fs", k=40, m=2),
+  data = df_par,
+  family = betar(link="logit"),
+  method = "fREML",
+  discrete = T
+)
+summary(fit_X)
+plot(fit_X)
+
+
+# Fit Post-Base FA ----
+#
+# Rather than treating sess as different groups,
+# account for intrasubject variance via Post-Base.
+#
+# Difference smooth is not flat f=29.80, 
+# expl dev = 36.6%.
+df_par <- df_par[, c("subj_id", "sess_name", "node_id", "dti_fa")]
+df_wide <- reshape(
+  df_par, 
+  timevar = "sess_name", 
+  idvar = c("subj_id", "node_id"),
+  direction="wide"
+)
+df_wide <- df_wide[, c("subj_id", "node_id", "dti_fa.base", "dti_fa.post")]
+df_wide <- df_wide[complete.cases(df_wide),]
+df_wide$diff <- df_wide$dti_fa.post - df_wide$dti_fa.base
+
+hist(df_wide$diff)
+descdist(df_wide$diff, discrete = F) # normal
+
+fit_D <- bam(
+  diff ~ s(node_id, bs="tp", k=15) + s(subj_id, bs="re"),
+  data = df_wide,
+  family = gaussian(),
+  method = "fREML",
+  discrete = T
+)
+gam.check(fit_D, rep=1000)
+summary(fit_D)
+plot(fit_D)
+
+plot_D <- getViz(fit_D)
+pDiff <- draw_group_smooth_diff(plot_D, 1, tract)
+
+
+
